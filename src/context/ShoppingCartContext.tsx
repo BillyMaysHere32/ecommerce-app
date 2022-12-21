ebac52d1f46d4b90812a5c42b5ec8b547ffc2395
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-20 18:20:38 -0800

    added get item, increase/decrease/remove item logic for shopping cart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -16,23 +16,66 @@ type ShoppingCartContext = {
     removeFromCart: (id: number) => void;
 }
 
-const ShoppingCartContext = createContext({} as ShoppingCartContext)
-{/* contains ShoppingCartContext values as it's type */}
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+{/* contains values of ShoppingCartContext type */}
 
 export function useShoppingCart() {
-  return useContext(ShoppingCartContext)
+  return useContext(ShoppingCartContext);
+  {/* custom hook to access context */}
 }
 
 export function ShoppingCartProvider( { children }: ShoppingCartProviderProps) {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
-    {/* store cart info in useState, move this to custom hook for local storage as well*/}
+    {/* store cart items in useState, useState is of type CartItem array */}
 
     function getItemQuantity(id: number){
         return cartItems.find(item => item.id === id)?.quantity || 0 
     }
 
+    function increaseCartQuantity(id: number){
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null) {
+                return[...currItems, { id, quantity: 1 }];
+                {/* if item is not in cart, add in new item and assign quantity of 1 */}
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity + 1 };
+                    {/* if item is in cart increase quantity */}
+                    } else {
+                        return item;
+                    }
+                })
+            }
+        })
+    }
+
+    function decreaseCartQuantity(id: number){
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1) {
+                return currItems.filter(item => item.id ! == id);
+                {/* if item is in cart with quantity of 1, item will be removed */}
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return { ...item, quantity: item.quantity - 1 };
+                    {/* if item is in cart increase quantity */}
+                    } else {
+                        return item;
+                    }
+                })
+            }
+        })
+    }
+
+    function removeFromCart(id: number){
+        setCartItems(currItems => {
+            return currItems.filter(item => item.id ! == id);
+        })
+    }
+
     return (
-        <ShoppingCartContext.Provider value={{}}>
+        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
             {children}
         </ShoppingCartContext.Provider>
     )
