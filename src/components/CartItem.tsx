4dcdf2d6d692688bf6fee3fc17d243f53ac4dbea
BillyMaysHere32 <kgetz32@gmail.com>
2022-12-22 13:24:55 -0800

    created CartItem component to show items in shopping cart pop out. added item img and name. next will add price and total cost logic

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,34 @@
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import storeItems from "../data/items.json"
+import { Button, Stack } from "react-bootstrap"
+
+type CartItemProps = {
+    id: number;
+    quantity: number;
+}
+
+export function CartItem({id, quantity}: CartItemProps) {
+    const { removeFromCart } = useShoppingCart()
+    const item = storeItems.find(item => item.id === id)
+    if (item == null) return null
+
+    return (
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+          <img
+            src={item.imgUrl}
+            style={{ width: "125px", height: "75px", objectFit: "cover" }}
+          />
+          <div className="me-auto">
+            {/* Push price total to the right */}
+            <div>
+              {item.name}{" "}
+              {quantity > 1 && (
+                <span className="text-muted" style={{ fontSize: ".65rem" }}>
+                  x{quantity}
+                </span>
+              )}
+            </div>
+
+        </Stack>
+      )
+}
