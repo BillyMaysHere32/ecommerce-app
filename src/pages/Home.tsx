84ff446b4c6eb10e7d3128c94d12717eb79dd878
BillyMaysHere32 <kgetz32@gmail.com>
2023-01-30 11:27:05 -0800

    text edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import logo from "../imgs/basic.jpg";
+
 export function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <img 
+        alt='...'
+        src={logo}>
+        </img>
     </div>
   )
 }
